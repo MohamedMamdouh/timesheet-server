c67419f9679d9052c9935c5be98b31e4a14743fb
Mohamed Mamdouh <meldakhakhni.gts@emaar.ae>
2020-09-07 21:55:54 +0400

    feat: add user route with crud opertation

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // Include app startup modules
 require("dotenv/config"); // To use env files
 require("./config/database"); // DataBase connections Stablish
+
+// Initialize Express Server
+const express = require("express");
+const app = express();
+
+//Body Parser integration for Parse incoming request bodies
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routes
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.listen(3000, () => console.log("App listen on port: 3000"));
